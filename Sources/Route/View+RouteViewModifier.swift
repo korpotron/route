462ece6514f6f9b44b3a3e477ccca30ba448bbb2
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-08-03 01:05:12 +0200

    aliases

--- Sources/Route/View+RouteViewModifier.swift
+++ Sources/Route/View+RouteViewModifier.swift
@@ -13,27 +13,46 @@ public extension View {
             return .done
         }
     }
+}
 
-    @inlinable func route(_ data: (some RouteLink).Type, block: @escaping () -> RouteResult) -> some View {
-        route(data) { _ in
-            block()
-        }
-    }
-
-    @inlinable func route(_ data: (some RouteLink).Type, block: @escaping () -> Void) -> some View {
-        route(data) { _ in
+public extension View {
+    @inlinable func route(_ a: (some RouteLink).Type, block: @escaping () -> Void) -> some View {
+        route(a) { _ in
             block()
             return .done
         }
     }
 
     @inlinable func route(_ a: (some RouteLink).Type, _ b: (some RouteLink).Type, block: @escaping () -> Void) -> some View {
-        route(a, block: block)
+        self
+            .route(a, block: block)
             .route(b, block: block)
     }
 
     @inlinable func route(_ a: (some RouteLink).Type, _ b: (some RouteLink).Type, _ c: (some RouteLink).Type, block: @escaping () -> Void) -> some View {
-        route(a, block: block)
+        self
+            .route(a, block: block)
+            .route(b, block: block)
+            .route(c, block: block)
+    }
+}
+
+public extension View {
+    @inlinable func route(_ a: (some RouteLink).Type, block: @escaping () -> RouteResult) -> some View {
+        route(a) { _ in
+            block()
+        }
+    }
+
+    @inlinable func route(_ a: (some RouteLink).Type, _ b: (some RouteLink).Type, block: @escaping () -> RouteResult) -> some View {
+        self
+            .route(a, block: block)
+            .route(b, block: block)
+    }
+
+    @inlinable func route(_ a: (some RouteLink).Type, _ b: (some RouteLink).Type, _ c: (some RouteLink).Type, block: @escaping () -> RouteResult) -> some View {
+        self
+            .route(a, block: block)
             .route(b, block: block)
             .route(c, block: block)
     }
