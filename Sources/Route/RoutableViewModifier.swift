052210f3b6b54310686e0f451a1ba98f1254c829
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-08-02 17:30:46 +0200

    tmp

--- Sources/Route/RoutableViewModifier.swift
+++ Sources/Route/RoutableViewModifier.swift
@@ -1,18 +1,18 @@
-import SwiftUI
-
-struct RoutableViewModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        let store = Store()
-        let action = RouteAction(for: store)
-
-        return content
-            .environment(\.store, store)
-            .environment(\.route, action)
-    }
-}
-
-public extension View {
-    func routable() -> some View {
-        modifier(RoutableViewModifier())
-    }
-}
+//import SwiftUI
+//
+//struct RoutableViewModifier: ViewModifier {
+//    func body(content: Content) -> some View {
+//        let store = Store()
+//        let action = RouteAction(for: store)
+//
+//        return content
+//            .environment(\.store, store)
+//            .environment(\.route, action)
+//    }
+//}
+//
+//public extension View {
+//    func routable() -> some View {
+//        modifier(RoutableViewModifier())
+//    }
+//}
