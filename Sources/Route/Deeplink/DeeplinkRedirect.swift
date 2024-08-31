1212f869c509ebfa4dc554b98ccdad5d1bf904ad
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-08-31 14:42:16 +0200

    add deeplinks redirect

---
+++ Sources/Route/Deeplink/DeeplinkRedirect.swift
@@ -0,0 +1,11 @@
+import Route
+
+public struct DeeplinkRedirect: RouteLink {
+    public let result: RouteResult
+}
+
+public extension RouteLink where Self == DeeplinkRedirect {
+    static func deeplink(_ result: RouteResult) -> Self {
+        DeeplinkRedirect(result: result)
+    }
+}
---
+++ Sources/Route/Deeplink/InvalidDeeplinkRedirect.swift
@@ -0,0 +1,12 @@
+import Route
+import Foundation
+
+public struct InvalidDeeplinkRedirect: RouteLink {
+    public let url: URL
+}
+
+public extension RouteLink where Self == InvalidDeeplinkRedirect {
+    static func invalid(url: URL) -> Self {
+        InvalidDeeplinkRedirect(url: url)
+    }
+}
