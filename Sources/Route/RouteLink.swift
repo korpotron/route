b0c6bb48f4259f460ea2e6e5c81b567f663e8f1c
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-09-25 22:21:31 +0200

    avoid sendable

--- Sources/Route/RouteLink.swift
+++ Sources/Route/RouteLink.swift
@@ -1,3 +1,3 @@
 import Foundation
 
-public protocol RouteLink: Sendable {}
+public protocol RouteLink {}
--- Sources/Route/RouteResult.swift
+++ Sources/Route/RouteResult.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 
-public struct RouteResult: Sendable {
+public struct RouteResult {
     let value: [any RouteLink]
 }
 
 public extension RouteResult {
-    static let done = Self(value: [])
+    static var done: Self {
+        Self(value: [])
+    }
 
     static func next(_ links: RouteLink...) -> Self {
         Self(value: links)
